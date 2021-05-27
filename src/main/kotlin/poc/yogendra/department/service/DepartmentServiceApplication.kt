a822b911974a838239f76154e3ac2082ac07767c
yogendramashete <yogendra.mashete@gmail.com>
2021-05-27 16:32:19 +0530

    Initial commit

---
+++ src/main/kotlin/poc/yogendra/department/service/DepartmentServiceApplication.kt
@@ -0,0 +1,11 @@
+package poc.yogendra.department.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DepartmentServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DepartmentServiceApplication>(*args)
+}
---
+++ src/test/kotlin/poc/yogendra/department/service/DepartmentServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package poc.yogendra.department.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
